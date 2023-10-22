acd80d4781133b7a693910b060cdb2af0914e786
Mada <madalitsomnduwira9@gmail.com>
2023-10-22 13:17:54 +0200

    Clean innit

---
+++ app/src/main/java/com/trials/socials/Login.kt
@@ -0,0 +1,16 @@
+package com.trials.socials
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class Login : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
---
+++ app/src/main/java/com/trials/socials/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.trials.socials
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
